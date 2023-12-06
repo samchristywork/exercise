038c086b6a07c8f8e93d5240352c64a157cb5ef0
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-12-05 21:20:49 -0600

    feat: Add countdown function
    This simple function will serve as a countdown to indicate how long
    until the next set is due.

--- exercise.sh
+++ exercise.sh
@@ -10,3 +10,12 @@ if [ $# -eq 0 ]; then
     echo "Usage: $0 <command>"
     exit 1
 fi
+
+function countdown() {
+    secs=$1
+    while [ "$secs" -gt 0 ]; do
+        echo -ne "$secs\033[0K\r"
+        sleep 1
+        : $((secs--))
+    done
+}
