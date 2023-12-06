2e7bb24727ad4d0d0a77250bc8014d8b27417d3b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-12-06 17:06:44 -0600

    style: Apply formatting
    I don't have an auto-formatter set up for bash scripts, so I was using
    bad formatting up until now.

--- exercise.sh
+++ exercise.sh
@@ -3,7 +3,7 @@
 filename="$HOME/.exercise"
 
 if [ ! -f "$filename" ]; then
-    touch "$filename"
+  touch "$filename"
 fi
 
 if [ $# -eq 0 ]; then
@@ -20,68 +20,54 @@ if [ $# -eq 0 ]; then
 fi
 
 function countdown() {
-    secs=$1
-    while [ "$secs" -gt 0 ]; do
-        echo -ne "$secs\033[0K\r"
-        sleep 1
-        : $((secs--))
-    done
+  secs=$1
+  while [ "$secs" -gt 0 ]; do
+    echo -ne "$secs\033[0K\r"
+    sleep 1
+    : $((secs--))
+  done
 }
 
 case "$1" in
-    add)
-        if [ $# -ne 3 ]; then
-            echo "Usage: $0 add <exercise> <reps>"
-            exit 1
-        fi
-        echo "$(date '+%Y-%m-%d %H:%M:%S')	$3	$2" >> "$filename"
-        ;;
-    start)
-        if [ $# -ne 4 ]; then
-            echo "Usage: $0 start <exercise> <reps> <interval>"
-            exit 1
-        fi
-        while true; do
-            ./exercise.sh add "$2" "$3"
-            echo "Added $3 $2"
+  add)
+    if [ $# -ne 3 ]; then
+      echo "Usage: $0 add <exercise> <reps>"
+      exit 1
+    fi
+    echo "$(date '+%Y-%m-%d %H:%M:%S')	$3	$2" >> "$filename"
+    ;;
+  start)
+    if [ $# -ne 4 ]; then
+      echo "Usage: $0 start <exercise> <reps> <interval>"
+      exit 1
+    fi
+    while true; do
+      ./exercise.sh add "$2" "$3"
+      echo "Added $3 $2"
 
-            date=$(date +%Y-%m-%d)
-            grep "$date" "$filename" | grep "$2" | \
-              awk -F '\t' '
-                          BEGIN {sum=0}
-                          {sum+=$2}
-                          END {print sum, "today"}'
+      ./exercise.sh today "$2"
 
-            countdown "$4"
+      countdown "$4"
 
-            read -r -p "Continue? [y/n] " yn
-            case "$yn" in
-                [Yy]* ) continue;;
-                [Nn]* ) break;;
-                * ) continue;;
-            esac
-        done
-        ;;
-    goal)
-        if [ $# -ne 3 ]; then
-            echo "Usage: $0 goal <start time> <end time> <exercise> <start reps> <end reps>"
-        fi
+      read -r -p "Continue? [y/n] " yn
+      case "$yn" in
+        [Yy]* ) continue;;
+        [Nn]* ) break;;
+        * ) continue;;
+      esac
+    done
+    ;;
 
-        start_time="$(date -d "$2" +%s)"
-        end_time="$(date -d "$3" +%s)"
-        exercise="$4"
-        start_reps="$5"
-        end_reps="$6"
-        current_time="$(date +%s)"
-        ;;
-    show)
-        cat "$filename"
-        ;;
-    edit)
-        "$EDITOR" "$filename"
-        ;;
-    *)
-        echo "Unknown command: $1"
-        exit 1
-        ;;
+    start_time="$(date -d "$2" +%s)"
+
+  show)
+    cat "$filename"
+    ;;
+  edit)
+    "$EDITOR" "$filename"
+    ;;
+  *)
+    echo "Unknown command: $1"
+    exit 1
+    ;;
 esac
